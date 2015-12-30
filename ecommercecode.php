f5235b1f1af11a767d1fdb7825888bc762e4d2f6
Ali Naderi <ali@thenaderigroup.com>
2015-12-30 13:46:36 -0500

    ecom simplified

--- ecommercecode.php
+++ ecommercecode.php
@@ -20,32 +20,13 @@ dataLayer = [{
 
 ---------------------------------------------------------
 
-<!-- PHP EXAMPLE FOR CLICKBANK THANK YOU PAGE -->
-
-<!-- COOKIE SCRIPT -->
-
-		<?php 
-		//unset ($_COOKIE['sffvi']);
-		//$newvisitor = 1;
-		//exit;
-		if((!isset($_COOKIE['sffvi'])) || ($_COOKIE['sffvi']!='1')) {
-			setcookie('sffvi', '1', time() + (172800 * 30), "/"); // 86400 = 1 day, 172800 = 2 days
-			// WITH GOOGLE CONTENT EXPERIMENTS -- setcookie('sffvi', $_COOKIE['sffvi']+1, time() + (172800 * 30), "/"); 
-			$newvisitor = 1;
-		}
-		else
-		{
-			$newvisitor = 0;
-		}
-		?>
-
-<!-- END COOKIE SCRIPT -->
+<!-- PHP EXAMPLE FOR FIRST UPSELL PAGE -->
 
 
-<!-- THANK YOU PAGE SCRIPT -->
+<!-- FIRST UPSELL PAGE SCRIPT -->
 
 <?php 
-	//LOAD COOKIE SCRIPT
+	//LOAD COOKIE SCRIPT. THIS FILE CAN BE FOUND IN THE RESPOSITORY, AND MUST BE INCLUDED ON YOUR DOMAIN SERVER.
 	include 'checkreturning.php';
 
 	//FETCH SALES DATA FROM URL
@@ -56,21 +37,9 @@ dataLayer = [{
 	    if ($item==''){
         $amount='null';
         $code='null';
-    } else if ($item==16){
-        $amount=27;
-        $code='HDD';
-    } else if ($item==17){
-        $amount=37;
-        $code='HDD';
-    } else if ($item==22){
-        $amount=19;
-        $code='HDD';
-    } else if ($item==28){
-        $amount=29;
-        $code='HDD';
-    } else if ($item==31){
-        $amount=20;
-        $code='HDD';
+    } else if ($item==1){
+        $amount=000;
+        $code='NAME';
     }
  
 	//IMPLEMENT ECOMMERCE CODE
@@ -95,15 +64,4 @@ dataLayer = [{
 	}
 ?>
 
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5XT92F"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-XXXXXXX');</script>
-<!-- End Google Tag Manager -->
-
-
-<!-- END THANK YOU PAGE SCRIPT -->
+<!-- END FIRST UPSELL PAGE SCRIPT -->
